631640b4a742964836c4c6007cf0b4ea868de3b0
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-10-28 22:12:26 +0530

    analyze route is added in the routes directory

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
+const analyzeRoute = require("./routes/analyze");
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -8,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 //config cors
 app.use(cors());
 app.use(express.json()); //for parsing application/json
+
+// routes
+app.use("/api/analyze", analyzeRoute);
 
 app.listen(PORT, () => {
     console.log(`AI Writing app listening at http://localhost:${PORT}`);
